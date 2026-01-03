db57e8ccc97441aac50530803f4bad33f3154c5c
Dhinesh-1112 <dhinesh.boopathy@genetins.com>
2026-01-03 18:56:54 +0530

    api.js file fix

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,4 @@
-// Base API URL (notice /api included)
-export const API_BASE = "http://localhost:4000";
+export const API_BASE =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4000"
+    : "https://api.thehabitry.com";
